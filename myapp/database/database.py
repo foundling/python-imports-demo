e80bb2d613c4cb02c826ac8e042ccedd888336ce
Alex Ramsdell <alexramsdell@gmail.com>
2025-03-25 15:10:58 -0400

    seems to work with reflex and inside single module.  add settings.json, .env and database submodule. put submodule path in settings/env files so individual file config can interpret the module structure correctly outside of reflex.

--- myapp/database/database.py
+++ myapp/database/database.py
@@ -1,7 +1,13 @@
 import sqlite3
 
+from lib.lib import fn
+
 class Database():
 
     def __init__(self):
         self.con = sqlite3.connect('test.db')
+        print(fn())
 
+if __name__ == '__main__':
+    d = Database()
+    print(d)
