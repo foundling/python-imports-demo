6d80ef3186dc0fa64aae5441d479a5a11ddd4d59
Alex Ramsdell <alexramsdell@gmail.com>
2025-03-25 15:14:44 -0400

    import sibling module.

--- myapp/database/database.py
+++ myapp/database/database.py
@@ -1,12 +1,14 @@
 import sqlite3
 
 from lib.lib import fn
+from sibling import sibling_f
 
 class Database():
 
     def __init__(self):
         self.con = sqlite3.connect('test.db')
         print(fn())
+        print(sibling_f())
 
 if __name__ == '__main__':
     d = Database()
---
+++ myapp/database/sibling.py
@@ -0,0 +1,2 @@
+def sibling_f():
+    return 43
